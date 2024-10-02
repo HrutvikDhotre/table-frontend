501f114cfcb05f0f30946d7722fcdd524320d4f3
HrutvikDhotre <hrutvikdhotre10@gmail.com>
2024-10-02 20:08:20 +0530

    changes

--- utils.js
+++ utils.js
@@ -17,34 +17,47 @@
 //     }
 // }
 
+// function allowOnlyNumbersAndDot(event) {
+//     const inputValue = event.target.value
+//     const pattern = /^[0-9.]$/
+
+//     const controlKeys = ['Backspace', 'ArrowLeft', 'ArrowRight', 'Delete']
+//     if (controlKeys.includes(event.key)) {
+//         return
+//     }
+
+//     if (event.type === 'keydown') {
+//         const char = event.key
+//         if (!pattern.test(char)) {
+//             event.preventDefault()
+//         }
+//         if (inputValue.includes('.') && char === '.') {
+//             event.preventDefault()
+//         }
+//     }
+
+//     if (event.type === 'input') {
+//         const sanitizedValue = inputValue.replace(/[^0-9.]/g, '')
+//         const dotCount = (sanitizedValue.match(/\./g) || []).length
+//         if (dotCount > 1) {
+//             event.target.value = sanitizedValue.substring(0, sanitizedValue.lastIndexOf('.'))
+//         } else {
+//             event.target.value = sanitizedValue
+//         }
+//     }
+// }
+
+
+
 function allowOnlyNumbersAndDot(event) {
     const inputValue = event.target.value
-    const pattern = /^[0-9.]$/
+    const sanitizedValue = inputValue.replace(/[^0-9.]/g, '')
+    const dotCount = (sanitizedValue.match(/\./g) || []).length
 
-    const controlKeys = ['Backspace', 'ArrowLeft', 'ArrowRight', 'Delete']
-    if (controlKeys.includes(event.key)) {
-        return
-    }
-
-    if (event.type === 'keydown') {
-        const char = event.key
-        if (!pattern.test(char)) {
-            event.preventDefault()
-        }
-        if (inputValue.includes('.') && char === '.') {
-            event.preventDefault()
-        }
-    }
-
-    if (event.type === 'input') {
-        const sanitizedValue = inputValue.replace(/[^0-9.]/g, '')
-        const dotCount = (sanitizedValue.match(/\./g) || []).length
-        if (dotCount > 1) {
-            event.target.value = sanitizedValue.substring(0, sanitizedValue.lastIndexOf('.'))
-        } else {
-            event.target.value = sanitizedValue
-        }
+    if (dotCount > 1) {
+        event.target.value = sanitizedValue.substring(0, sanitizedValue.lastIndexOf('.'))
+    } else {
+        event.target.value = sanitizedValue
     }
 }
-
 
